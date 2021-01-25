9e64f13826528eafde7fe14add6786d99a8a6f78
Arlen Manasov <59771669+TossACoinToUrProgrammer@users.noreply.github.com>
2021-01-25 20:39:48 +0600

    Changed BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 
@@ -10,9 +10,9 @@ import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById("root")
 );
